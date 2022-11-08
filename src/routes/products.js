4b614b3b8213d6be3b819fcef25f5b3ac4087ec5
ayancarelli <alan_yancarelli@hotmail.com>
2022-11-08 19:21:34 -0300

    img crear equipo funcional

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const productsController = require('../controllers/productsController');
+
+const storage = multer.diskStorage({
+    destination: (req, file,cb) =>{
+        cb(null,path.join(__dirname,'../../public/img/img-equipos'));
+    },
+    filename: (req,file,cb) =>{
+        
+        console.log(file);
+        const newFileName = 'img-equipo-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFileName);
+    }
+});
+
+const upload = multer ({storage: storage});
+
+
 
 router.get ('/canchas', productsController.canchas);
 router.get ('/equipos', productsController.equipos);
 router.get ('/carrito', productsController.carrito);
 router.get ('/carrito2', productsController.carrito2);
 router.get ('/crear-equipo', productsController.create);
-router.post ('/crear-equipo', productsController.crear);
+router.post ('/crear-equipo', upload.single("imgEquipo"), productsController.crear);
 router.get ('/equipo/:id', productsController.equipo);
 
 /*** Para Editar equipos ***/
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const usersController = require('../controllers/usersController');
+
+const storage = multer.diskStorage({
+    destination: (req,file,cb) =>{
+        cb(null,path.join(__dirname,'../public/img/img-users'))
+    },
+    filename: (req,file,cb) =>{
+        const newFileName = 'img-user-' + Date.now() + path.extname(file.originalname);
+        cb(null,newFileName);
+    }
+});
+
+const upload = multer ({storage: storage});
 
 router.get ('/login', usersController.login);
 router.post ('/login', usersController.checkUser);
 router.get ('/registro',usersController.registro);
-router.post ('/registro',usersController.crear);
+router.post ('/registro',upload.single('fotoPerfil') ,usersController.crear);
 
 // --- EN VEREMOS ESTA VISTA ---
 // router.get('/users',usersController.users);
