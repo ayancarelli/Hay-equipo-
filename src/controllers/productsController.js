e765f67b091e78b33f13b16f621b2be4c2f847a3
Frithureiks73 <fedex_73@hotmail.com>
2022-12-04 16:32:20 -0300

    empezando a acomodar equipos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,36 +9,35 @@ const equiposJson = JSON.parse(fs.readFileSync(equiposFilePath, 'utf-8'));
 
 const controlador = {
     canchas: (req, res) => {
-        res.render('./products/canchas', {moment: moment});
+        res.render('./products/canchas', { moment: moment });
     },
 
-    equipos: (req,res) => {
-        //const equiposJson = JSON.parse(fs.readFileSync(equiposFilePath, 'utf-8'));
-        //res.render('./products/equipos', {ps: equiposJson});
+    equipos: (req, res) => {        
         db.equipo.findAll().then((equipos) => {
-
             res.render('./products/equipos', { ps: equipos });
         });
     },
 
-    carrito: (req,res) => {
+    carrito: (req, res) => {
         res.render('./products/carrito');
     },
 
-    carrito2: (req,res) => {
+    carrito2: (req, res) => {
         res.render('./products/carrito2');
     },
 
-    create: (req,res) => {
-        res.render('products/crear-equipo');
+    create: async (req, res) => {
+        let restricciones = await db.tipo_restriccion.findAll({include: [{ association: 'restriccion' }]})
+        
+        res.render('products/crear-equipo', { restricciones: restricciones});
     },
 
-    crear: (req,res)=> {
+    crear: (req, res) => {
         let newId;
-        if(equiposJson.length>0){
-            newId = equiposJson[(equiposJson.length-1)].id+1;
+        if (equiposJson.length > 0) {
+            newId = equiposJson[(equiposJson.length - 1)].id + 1;
         } else {
-            newId = 1    
+            newId = 1
         }
         let equipoNuevo = {
             id: newId,
@@ -53,87 +52,73 @@ const controlador = {
             restriccionEdad: req.body.restriccionEdad,
             restriccionesSexo: req.body.restriccionesSexo
         };
-        
+
         equiposJson.push(equipoNuevo);
-        
-        fs.writeFileSync(equiposFilePath,JSON.stringify(equiposJson, null, " "));
+
+        fs.writeFileSync(equiposFilePath, JSON.stringify(equiposJson, null, " "));
         console.log(equipoNuevo);
         res.redirect('/products/equipos');
     },
 
-    edit: (req,res) => {
-        let idEquipo = req.params.id;
-		let objEquipo;
-
-		for (let e of equiposJson){
-			if (idEquipo == e.id){
-				objEquipo=e;
-				break;
-			}
-		}
-
-		res.render('./products/editar-equipo',{equipo: objEquipo})
-    },
-    
-    equipo: (req,res) => {
-        let idEquipo = req.params.id;
-        let objEquipo;
-
-        for (let e of equiposJson){
-            if (idEquipo == e.id){
-                objEquipo=e;
-                break
-
-            }
-        }
-        /* db.equipo.findOne({
-            where:{
+    edit: (req, res) => {
+        db.equipo.findOne({
+            where: {
                 id: req.params.id
             }
-        }) */
-        
-        res.render('./products/equipo', {equipo: objEquipo})
+        }).then((objEquipo) => {
+            res.render('./products/editar-equipo', { equipo: objEquipo })
+        })
     },
-    
-    update: (req,res) => {
-   
+
+    equipo: (req, res) => {
+        db.equipo.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then((objEquipo) => {
+            res.render('./products/equipo', { equipo: objEquipo })
+        })
+    },
+
+    update: (req, res) => {
+
         let idEquipo = req.params.id;
-   
-        for (let e of equiposJson){
-            if (idEquipo == e.id){
-                
-                e.nombreEquipo= req.body.nombreEquipo.toUpperCase();
-                e.jugador1= req.body.jugador1.toUpperCase();
-                e.jugador2= req.body.jugador2.toUpperCase();
-                e.jugador3= req.body.jugador3.toUpperCase();
-                e.jugador4= req.body.jugador4.toUpperCase();
-                e.jugador5= req.body.jugador5.toUpperCase();
-                e.jugador6= req.body.jugador6.toUpperCase();
-                e.restriccionEdad= req.body.restriccionEdad;
-                e.restriccionesSexo= req.body.restriccionesSexo;
+
+        for (let e of equiposJson) {
+            if (idEquipo == e.id) {
+
+                e.nombreEquipo = req.body.nombreEquipo.toUpperCase();
+                e.jugador1 = req.body.jugador1.toUpperCase();
+                e.jugador2 = req.body.jugador2.toUpperCase();
+                e.jugador3 = req.body.jugador3.toUpperCase();
+                e.jugador4 = req.body.jugador4.toUpperCase();
+                e.jugador5 = req.body.jugador5.toUpperCase();
+                e.jugador6 = req.body.jugador6.toUpperCase();
+                e.restriccionEdad = req.body.restriccionEdad;
+                e.restriccionesSexo = req.body.restriccionesSexo;
                 break;
 
             }
         }
-        fs.writeFileSync(equiposFilePath,JSON.stringify(equiposJson, null, " "));
-      
+        fs.writeFileSync(equiposFilePath, JSON.stringify(equiposJson, null, " "));
+
         res.redirect('/products/equipos');
     },
 
     destroy: (req, res) => {
-		
-		let idEquipo = req.params.id;
 
-		let arrEquipos = equiposJson.filter(function(elemento){
-			return elemento.id!=idEquipo;
-		});
-		
-		fs.writeFileSync(
+        let idEquipo = req.params.id;
+
+        let arrEquipos = equiposJson.filter(function (elemento) {
+            return elemento.id != idEquipo;
+        });
+
+        fs.writeFileSync(
             path.join(__dirname, '../data/equiposDataBase.json'),
-            JSON.stringify(arrEquipos,null," "));
-            
-        res.redirect('/');        
-	}
+            JSON.stringify(arrEquipos, null, " "));
+
+        res.redirect('/');
+    }
 }
 
 module.exports = controlador;
--- src/database/models/tipo_restriccion.js
+++ src/database/models/tipo_restriccion.js
@@ -2,7 +2,7 @@ function tipoRestriccionData(sequelize, Datatypes) {
     alias = 'tipo_restriccion';
     cols = {
         id: { type: Datatypes.TINYINT(4).UNSIGNED, primaryKey: true, autoIncrement: true, allowNull: false },
-        descripcion: { type: Datatypes.STRING(20), allowNull: false }
+        descripcion: { type: Datatypes.STRING(100), allowNull: false }
     }
 
     config = { freezeTableName: true, timestamps: false, camelCase: false };
