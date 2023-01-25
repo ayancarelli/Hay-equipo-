d1fe2a443ea1356d13618f2cab2c923af08eaca8
Frithureiks73 <fedex_73@hotmail.com>
2023-01-25 11:20:58 -0300

    arreglando detalles

--- src/routes/usersApi.js
+++ src/routes/usersApi.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const usersControllerApi = require('../controllers/usersControllerApi');
 
 
-
+router.get('/todos', usersControllerApi.todos);
 router.get('/', usersControllerApi.list);
 router.get('/:id', usersControllerApi.show);
 
